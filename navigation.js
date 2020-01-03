1a6bf25240e8b886c02a7053d5f424b9313c18cf
Alex <fre3run@gmail.com>
2020-01-03 17:07:22 +0200

    added login + register screens

--- navigation.js
+++ navigation.js
@@ -3,7 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Screens from './screens';
 
 const Navigator = createStackNavigator({
-    Login: { screen: Screens.Login }
+    LoginScreen: { screen: Screens.Login },
+    RegisterScreen: { screen: Screens.Register }
 });
 
+
 export default createAppContainer(Navigator);
+
+export const ScreenNames = {
+    LoginScreen: 'LoginScreen',
+    RegisterScreen: 'RegisterScreen'
+};
--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { ScreenNames } from '../../navigation';
 
-const Login = (props) => {
+const Login = ({ navigation }) => {
+
+    const onRegister = useCallback(() => {
+        navigation.navigate(ScreenNames.RegisterScreen);
+    }, []);
+
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <TextInput
+                style={styles.textInput}
+                placeholder={'E-mail'}
+                keyboardType={'email-address'}
+            />
+            <TextInput
+                style={styles.textInput}
+                placeholder={'Password'}
+                secureTextEntry
+            />
+            <View style={styles.buttonContainer}>
+                <Button
+                    title={'login'} />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    onPress={onRegister}
+                    title={'register'} />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+    },
+    textInput: {
+        width: 200,
+        borderWidth: 1,
+        marginTop: 10,
+        padding: 15
+    },
+    buttonContainer: {
+        marginTop: 10
+    }
+});
 
 export default Login;
